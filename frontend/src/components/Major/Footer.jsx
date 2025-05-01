efcc0766626a8f788e7d7b4ca22586c1ffbf9892
eiechuang <119470862+eiechuang@users.noreply.github.com>
2025-05-01 16:37:24 -0700

    Add files via upload

--- frontend/src/components/Major/Footer.jsx
+++ frontend/src/components/Major/Footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
-    <div className="flex justify-center items-center">
-      <p>Some footer text</p>
+    <div className="flex justify-center bg-blue-800 py-4">
+      <p className="text-white font-mono"> Spartan Food Store © 2025</p>
+    
     </div>
   );
 }
