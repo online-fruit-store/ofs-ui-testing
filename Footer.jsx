5d547cc0929e8b7ac332d32bad05b9423459b324
eiechuang <119470862+eiechuang@users.noreply.github.com>
2025-05-01 16:36:11 -0700

    Delete Footer.jsx

--- Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="flex justify-center bg-blue-800 py-4">
-      <p className="text-white font-mono"> Spartan Food Store © 2025</p>
-    
-    </div>
-  );
-}
